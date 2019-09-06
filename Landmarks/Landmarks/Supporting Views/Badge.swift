94769acc87f5562bca96b91ad8f1602cbfa59eb3
tdrk18 <tdrk18@gmail.com>
2019-09-06 19:44:04 +0900

    Create a Badge View

---
+++ Landmarks/Landmarks/Supporting Views/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by tdrk18 on 2019/09/06.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
